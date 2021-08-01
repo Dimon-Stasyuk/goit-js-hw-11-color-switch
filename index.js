f47bff90fdbf99d0033a074b9b096d07763caf4e
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-08-01 10:09:30 +0300

    пофиксил баг с кнопкой start

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
 ];
 
 const refs = {
-    startBtn: document.querySelector('[data-action="start"]'),
-    stopBtn: document.querySelector('[data-action="stop"]'),
-}
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
 const { startBtn, stopBtn } = refs;
 let isActive = false;
 let timerId = null;
 
-startBtn.addEventListener('click', onStartBtn);
-stopBtn.addEventListener('click', onStopBtn);
+startBtn.addEventListener("click", onStartBtn);
+stopBtn.addEventListener("click", onStopBtn);
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-
 function onStartBtn() {
-    if (isActive === false) {        
-        colorChoice();
-    }    
+  if (isActive === false) {
+    colorChoice();
+    startBtn.disabled = true;
+  }
 }
 
 function onStopBtn() {
-    clearInterval(timerId);
-    isActive = false;
+  clearInterval(timerId);
+  isActive = false;
+  startBtn.disabled = false;
 }
 
 function colorChoice() {
-    timerId = setInterval(() => {
-        isActive = true;
-        document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
-        }, 1000);
+  timerId = setInterval(() => {
+    isActive = true;
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
 }
